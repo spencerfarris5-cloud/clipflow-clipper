32ee934218ce529be64d8ac5756729b3db0cd92a
spencerfarris5-cloud <spencerfarris5@gmail.com>
2026-09-11 15:52:59 -0400

    fix: use android client for H.264 + force encoder threads=1 to stop OOM

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post("/clip", auth, async (req, res) => {
     await run("yt-dlp", [
       "--download-sections", section,
       "--force-keyframes-at-cuts",
-      "--extractor-args", "youtube:player_client=web",
+      "--extractor-args", "youtube:player_client=android",
       "-f", "bestvideo[height<=720][vcodec^=avc1]+bestaudio[acodec^=mp4a]/best[height<=720][vcodec^=avc1]/best[height<=720]/best",
       "--merge-output-format", "mp4",
       "--no-playlist",
@@ -92,7 +92,7 @@ app.post("/clip", auth, async (req, res) => {
       "-y", "-threads", "1", "-i", rawPath,
       "-vf", vf,
       "-r", "30",
-      "-c:v", "libx264", "-x264-params", "threads=2", "-preset", "veryfast", "-crf", "23",
+      "-c:v", "libx264", "-threads", "1", "-preset", "veryfast", "-crf", "23",
       "-c:a", "aac", "-b:a", "128k",
       "-movflags", "+faststart",
       outPath
